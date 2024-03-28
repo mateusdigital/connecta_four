ebb5725d1b43e663d292c9a4cb18ce77a8a3420c
mateusdigital <hello@mateus.digital>
2024-03-28 17:35:46 -0300

    Add some quick assets.

---
+++ client/assets/texture-defs.js
@@ -0,0 +1,63 @@
+const ASSETS_CHARACTERS_CHARACTERS_0001 = "assets/characters/characters_0001.png"
+const ASSETS_CHARACTERS_CHARACTERS_0002 = "assets/characters/characters_0002.png"
+const ASSETS_CHARACTERS_CHARACTERS_0003 = "assets/characters/characters_0003.png"
+const ASSETS_CHARACTERS_CHARACTERS_0004 = "assets/characters/characters_0004.png"
+const ASSETS_CHARACTERS_CHARACTERS_0005 = "assets/characters/characters_0005.png"
+const ASSETS_CHARACTERS_CHARACTERS_0006 = "assets/characters/characters_0006.png"
+const ASSETS_CHARACTERS_CHARACTERS_0007 = "assets/characters/characters_0007.png"
+const ASSETS_UI_ASSETS_BG = "assets/ui_assets/bg.png"
+const ASSETS_UI_ASSETS_BG_EFFECT_FLARE = "assets/ui_assets/bg_effect_flare.png"
+const ASSETS_UI_ASSETS_BG_OVERLAY = "assets/ui_assets/bg_overlay.png"
+const ASSETS_UI_ASSETS_BUTTON_9SLICE = "assets/ui_assets/button_9slice.png"
+const ASSETS_UI_ASSETS_BUTTON_9SLICE01 = "assets/ui_assets/button_9slice01.png"
+const ASSETS_UI_ASSETS_BUTTON_AUDIOMUTE = "assets/ui_assets/button_audioMute.png"
+const ASSETS_UI_ASSETS_BUTTON_AUDIOUNMUTE = "assets/ui_assets/button_audioUnMute.png"
+const ASSETS_UI_ASSETS_BUTTON_CLOSE01 = "assets/ui_assets/button_close01.png"
+const ASSETS_UI_ASSETS_BUTTON_CLOSE02 = "assets/ui_assets/button_close02.png"
+const ASSETS_UI_ASSETS_BUTTON_DIRECTION01 = "assets/ui_assets/button_direction01.png"
+const ASSETS_UI_ASSETS_BUTTON_DIRECTION02 = "assets/ui_assets/button_direction02.png"
+const ASSETS_UI_ASSETS_BUTTON_INFO01 = "assets/ui_assets/button_info01.png"
+const ASSETS_UI_ASSETS_BUTTON_INFO02 = "assets/ui_assets/button_info02.png"
+const ASSETS_UI_ASSETS_BUTTON_REPLAY01 = "assets/ui_assets/button_replay01.png"
+const ASSETS_UI_ASSETS_GAME_TITLE = "assets/ui_assets/game_title.png"
+const ASSETS_UI_ASSETS_ICONLEVEL = "assets/ui_assets/iconLevel.png"
+const ASSETS_UI_ASSETS_ICON_INFO = "assets/ui_assets/icon_info.png"
+const ASSETS_UI_ASSETS_INGAMEBANKBALANCE_BAR = "assets/ui_assets/inGameBankBalance_bar.png"
+const ASSETS_UI_ASSETS_INGAMECOUNTER_BAR = "assets/ui_assets/inGameCounter_bar.png"
+const ASSETS_UI_ASSETS_INGAME_COUNTER = "assets/ui_assets/inGame_counter.png"
+const ASSETS_UI_ASSETS_POPUP_BASE = "assets/ui_assets/popup_base.png"
+const ASSETS_UI_ASSETS_POPUP_HEADER = "assets/ui_assets/popup_header.png"
+const ASSETS_UI_ASSETS_SHOP_TITLE = "assets/ui_assets/shop_title.png"
+const ASSETS_UI_ASSETS_STORECOUNTER_BAR = "assets/ui_assets/storeCounter_bar.png"
+
+const TEXTURES_TO_LOAD = [ "assets/characters/characters_0001.png",
+"assets/characters/characters_0002.png",
+"assets/characters/characters_0003.png",
+"assets/characters/characters_0004.png",
+"assets/characters/characters_0005.png",
+"assets/characters/characters_0006.png",
+"assets/characters/characters_0007.png",
+"assets/ui_assets/bg.png",
+"assets/ui_assets/bg_effect_flare.png",
+"assets/ui_assets/bg_overlay.png",
+"assets/ui_assets/button_9slice.png",
+"assets/ui_assets/button_9slice01.png",
+"assets/ui_assets/button_audioMute.png",
+"assets/ui_assets/button_audioUnMute.png",
+"assets/ui_assets/button_close01.png",
+"assets/ui_assets/button_close02.png",
+"assets/ui_assets/button_direction01.png",
+"assets/ui_assets/button_direction02.png",
+"assets/ui_assets/button_info01.png",
+"assets/ui_assets/button_info02.png",
+"assets/ui_assets/button_replay01.png",
+"assets/ui_assets/game_title.png",
+"assets/ui_assets/iconLevel.png",
+"assets/ui_assets/icon_info.png",
+"assets/ui_assets/inGameBankBalance_bar.png",
+"assets/ui_assets/inGameCounter_bar.png",
+"assets/ui_assets/inGame_counter.png",
+"assets/ui_assets/popup_base.png",
+"assets/ui_assets/popup_header.png",
+"assets/ui_assets/shop_title.png",
+"assets/ui_assets/storeCounter_bar.png" ]
